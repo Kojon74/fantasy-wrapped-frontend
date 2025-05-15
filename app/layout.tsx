57ea8f3fe657a67e1507b1185a2c0eff5ccabde6
Ken Johnson <dragonkenkj@gmail.com>
2025-05-14 23:27:30 -0700

    Added Next.js analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
 import { NavBar } from "@/components/NavBar";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Fantasy Warped",
@@ -24,6 +25,7 @@ export default function RootLayout({
         <Providers>
           <NavBar />
           {children}
+          <Analytics />
         </Providers>
       </body>
     </html>
